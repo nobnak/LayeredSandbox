22c301fad77e4a1e8471e9ca73ac44af711aa4da
Nobuyuki Nakata <nobu.nakata@gmail.com>
2017-04-18 17:17:43 +0900

    カメラとレイヤー間の距離を、正規化したものと実施の値と、選べるように

--- Scripts/BackgroundFullscreenPlane.cs
+++ Scripts/BackgroundFullscreenPlane.cs
@@ -6,12 +6,19 @@ namespace PointRegistrationSubmod {
         
     [ExecuteInEditMode]
     public class BackgroundFullscreenPlane : BasePlane {
+        public enum DepthModeEnum { Normalized = 0, Exact }
+
+        public Camera targetCam;
+
+        public DepthModeEnum depthMode;
+        [Header("Normalized Depth")]
     	[Range(0f, 0.99f)]
     	public float normalizedDepth = 0f;
-    	public Camera targetCam;
+        [Header("Exact Depth")]
+        public float depth = 10f;
 
         void Update() {
-            var depth = Mathf.Lerp (targetCam.nearClipPlane, targetCam.farClipPlane, normalizedDepth);
+            var depth = Depth ();
     		var scale = targetCam.ViewportToWorldPoint (new Vector3 (1f, 1f, depth))
     			- targetCam.ViewportToWorldPoint (new Vector3 (0f, 0f, depth));
     		scale = targetCam.transform.InverseTransformDirection (scale);
@@ -20,6 +27,15 @@ namespace PointRegistrationSubmod {
             transform.position = targetCam.ViewportToWorldPoint (new Vector3 (0.5f, 0.5f, depth));
             transform.rotation = targetCam.transform.rotation;
     		transform.localScale = scale;
+        }
+
+        float Depth() {
+            switch (depthMode) {
+            case DepthModeEnum.Normalized:
+                return Mathf.Lerp (targetCam.nearClipPlane, targetCam.farClipPlane, normalizedDepth);
+            default:
+                return depth;
+            }
         }
     }
 }
